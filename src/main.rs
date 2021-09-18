6a787a61499ca94c86a3411e9c294676e796d1da
nick <pavlov_na@citis.ru>
2021-09-18 15:07:04 +0300

    Added simple vga text output

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,24 @@
 
 use core::panic::PanicInfo;
 
-#[no_mangle]
-pub extern "C" fn _start() -> ! {
-    loop {}
-}
+static GREET: &[u8] = b"CRUCIFIX";
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    let vga_buffer = 0xB8000 as *mut u8;
+
+    GREET
+        .iter()
+        .flat_map(|bt| [*bt, 0xF as u8])
+        .enumerate()
+        .for_each(|(i, byte)| unsafe {
+            *vga_buffer.offset(i as isize) = byte;
+        });
+
     loop {}
 }
